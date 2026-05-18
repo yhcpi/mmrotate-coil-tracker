6ec9f666be968d2212a4360404f1210aa783d6cf
Peter <13787086860@163.com>
2026-05-18 18:19:48 +0800

    retinanet训练初步配置

---
+++ configs/rotated_retinanet/rotated_retinanet_r18_fpn_1x_rail_le90.py
@@ -0,0 +1,124 @@
+_base_ = [
+    '../_base_/default_runtime.py'
+]
+angle_version = 'le90'
+model = dict(
+    type='RotatedRetinaNet',
+    backbone=dict(
+        type='ResNet',
+        depth=18,                       # ResNet18 更轻更快
+        num_stages=4,
+        out_indices=(0, 1, 2, 3),
+        frozen_stages=0,
+        norm_cfg=dict(type='BN', requires_grad=True),
+        norm_eval=True,
+        style='pytorch',
+        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet18')),
+    neck=dict(
+        type='FPN',
+        in_channels=[64, 128, 256, 512],
+        out_channels=128,
+        start_level=1,
+        add_extra_convs='on_input',
+        num_outs=5),
+    bbox_head=dict(
+        type='RotatedRetinaHead',
+        num_classes=1,                   # 你的钢轨只有1类
+        in_channels=128,
+        stacked_convs=2,                 # 背景简单，2层卷积足够
+        feat_channels=128,
+        anchor_generator=dict(
+            type='RotatedAnchorGenerator',
+            octave_base_scale=4,
+            scales_per_octave=1,         # 钢轨尺寸固定，1种尺度
+            ratios=[1.0],                # 钢轨比例固定
+            strides=[8, 16, 32, 64, 128]),
+        bbox_coder=dict(
+            type='DeltaXYWHAOBBoxCoder',
+            angle_range=angle_version,
+            norm_factor=None,
+            edge_swap=True,
+            proj_xy=True,
+            target_means=(.0, .0, .0, .0, .0),
+            target_stds=(1.0, 1.0, 1.0, 1.0, 1.0)),
+        loss_cls=dict(
+            type='FocalLoss',
+            use_sigmoid=True,
+            gamma=2.0,
+            alpha=0.25,
+            loss_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=1.0)),
+    train_cfg=dict(
+        assigner=dict(
+            type='MaxIoUAssigner',
+            pos_iou_thr=0.5,
+            neg_iou_thr=0.4,
+            min_pos_iou=0,
+            ignore_iof_thr=-1,
+            iou_calculator=dict(type='RBboxOverlaps2D')),
+        allowed_border=-1,
+        pos_weight=-1,
+        debug=False),
+    test_cfg=dict(
+        nms_pre=2000,
+        min_bbox_size=0,
+        score_thr=0.05,
+        nms=dict(iou_thr=0.1),
+        max_per_img=2000))
+# ===== 数据集配置 =====
+data_root = 'data/rail/'
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(type='RResize', img_scale=(640, 640)),     # 小一点更快
+    dict(type='RRandomFlip', flip_ratio=0.5),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='Pad', size_divisor=32),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels'])
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(640, 640),
+        flip=False,
+        transforms=[
+            dict(type='RResize'),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='Pad', size_divisor=32),
+            dict(type='DefaultFormatBundle'),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data = dict(
+    samples_per_gpu=8,              # 4060 Ti 16GB → 批次拉到8
+    workers_per_gpu=2,
+    train=dict(
+        type='RailDataset',
+        ann_file=data_root + 'train/annfiles/',
+        img_prefix=data_root + 'train/images/',
+        pipeline=train_pipeline,
+        version=angle_version),
+    val=dict(
+        type='RailDataset',
+        ann_file=data_root + 'val/annfiles/',
+        img_prefix=data_root + 'val/images/',
+        pipeline=test_pipeline,
+        version=angle_version),
+    test=dict(
+        type='RailDataset',
+        ann_file=data_root + 'test/annfiles/',
+        img_prefix=data_root + 'test/images/',
+        pipeline=test_pipeline,
+        version=angle_version))
+# ===== 训练计划 =====
+optimizer = dict(type='SGD', lr=0.001, momentum=0.9)
+lr_config = dict(policy='step', step=[8, 11])
+runner = dict(type='EpochBasedRunner', max_epochs=12)
+checkpoint_config = dict(interval=1)
+log_config = dict(interval=50, hooks=[dict(type='TextLoggerHook')])
